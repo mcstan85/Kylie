c16c5a59e205cfb2aca24c2d8e401ceb2b42709c
Levi <mstan6672@gmail.com>
2022-12-19 19:37:28 +0530

    ✨мαяк  ᴍᴜsɪᴄ✨

---
+++ MarkMusic/utils/inline/start.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO
+from YukkiMusic import app
+
+
+def start_pannel(_):
+    return
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):   
+    return 
