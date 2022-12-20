773e1e973a8dc97ebc9b6d63542bc613d7c8e4f8
Levi <115207729+mcstan85@users.noreply.github.com>
2022-12-20 14:39:09 +0530

    Update play.py

--- MarkMusic/plugins/play/play.py
+++ MarkMusic/plugins/play/play.py
@@ -392,11 +392,9 @@ async def play_commnd(
             if details["duration_min"]:
                 duration_sec = time_to_seconds(
                     details["duration_min"]
-                 )
+                )
+                if not await is_approved(user_id):
                     if duration_sec > config.DURATION_LIMIT:
-                        return await mystic.edit_text(
-                            _["play_6"].format(
-                                config.DURATION_LIMIT_MIN,
                                 details["duration_min"],
                             )
                         )
