75b38776cc00411ce8230960e73843a7a20a3e29
Levi <115207729+mcstan85@users.noreply.github.com>
2023-01-09 13:57:39 +0530

    Update start.py

--- MarkMusic/utils/inline/start.py
+++ MarkMusic/utils/inline/start.py
@@ -41,22 +41,23 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="» Aᴅᴅ ᴍᴇ ʙᴀʙʏ «",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             ),
-        ],
+        ], 
         [
             InlineKeyboardButton(
-                text="•ʜᴇʟᴩ•", callback_data="settings_back_helper"
+                text="•ʜᴇʟᴩ•",
+                callback_data="settings_back_helper",
             ),
-            InlineKeyboardButton(text="•ᴅᴇᴠᴇʟᴏᴘᴇʀ•", url=f"https://t.me/marrk_85"),
         ],
         [ 
             InlineKeyboardButton(text="•ᴄʜᴀɴɴᴇʟ•", url=f"{config.SUPPORT_CHANNEL}"),
             InlineKeyboardButton(text="•ɢʀᴏᴜᴩ•", url=f"{config.SUPPORT_GROUP}"),
-        ],
+        ],       
         [
-            InlineKeyboardButton(text="•sᴏᴜʀᴄᴇ•", url=f"{config.UPSTREAM_REPO}"),
-            InlineKeyboardButton(text="•Mᴏɪ ᴏᴡɴᴇʀ•", user_id=OWNER)
-        ],
-     ]
+            InlineKeyboardButton(
+                text="•Mᴏɪ ᴏᴡɴᴇʀ•",
+                user_id=OWNER)
+        ],     
+        ]
     return buttons
 
 
